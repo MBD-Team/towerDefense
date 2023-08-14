92880cbe915f483c92e36be1e8ec514d20af29fd
tim-labahn <tim.labahn@freaknet.de>
2023-08-14 10:49:29 +0200

    render and a little more

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './style.css';
 type GameMap = {
   isPlayerTower: boolean;
-  playerBase: boolean;
-  enemyStart: boolean;
-  enemyPath: boolean;
+  isPlayerBase: boolean;
+  isEnemyStart: boolean;
+  isEnemyPath: boolean;
   isEnemy: boolean;
 }[][];
 
@@ -16,4 +16,27 @@ function renderMap() {
   if (gameField !== null) {
     gameField.innerHTML = '';
   }
+  gameField?.setAttribute('style', `grid-template-columns: repeat(${gameSize},1fr); width: ${50 * gameSize}px;`);
+  for (let x = 0; x !== gameSize; x++) {
+    for (let y = 0; y !== gameSize; y++) {
+      const tile = document.createElement('div');
+      tile.className = 'tile';
+      tile.onclick = () => {
+        tileClick(x, y);
+      };
+      if (gameMap[x][y].isPlayerTower) {
+        tile.setAttribute('isPlayerTower', 'PlayerTower');
+        tile.innerHTML = '🔫';
+      }
+      if (gameMap[x][y].isPlayerBase) {
+        tile.setAttribute('isPlayerTower', 'PlayerTower');
+        tile.innerHTML = '';
+      }
+    }
+  }
+}
+
+function tileClick(IndexX: number, IndexY: number) {
+  IndexX;
+  IndexY;
 }
