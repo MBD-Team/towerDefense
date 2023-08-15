130d5e603a28f69de02aafd4dcae0c5cd92dd22b
tim-labahn <tim.labahn@freaknet.de>
2023-08-15 11:24:14 +0200

    tower

--- src/main.ts
+++ src/main.ts
@@ -103,7 +103,7 @@ function renderMap() {
         tileClick(x, y);
       };
       if (gameMap[x][y].isPlayerTower) {
-        tile.innerHTML = '🔫';
+        tile.classList.add('tower');
       }
       if (gameMap[x][y].isPlayerBase) {
         tile.innerHTML = '💻';
